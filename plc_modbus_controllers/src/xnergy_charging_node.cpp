afe06d29c9eeb77698ebfbf6addf040f782f52c7
Shrey B <shreyasbabu97@gmail.com>
2021-08-02 16:54:45 +0800

    xnergy charging node

--- plc_modbus_controllers/src/xnergy_charging_node.cpp
+++ plc_modbus_controllers/src/xnergy_charging_node.cpp
@@ -12,9 +12,32 @@ using plc_modbus_node::xnergy_sensors;
 xnergy_sensors xn_sensors;
 
 ros::Subscriber sensors_read;
-ros::Publisher regs_write;
+ros::Publisher coils_write;
 
-void sensors_callback(const forklift_sensors::ConstPtr& data){
+bool charge_cmds(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+{
+    if (fl_sensors.lift_cmd != 0 || fl_sensors.busy_status != false){
+        // Forklift is completing previous command, don't issue command
+        ROS_WARN("FORKLIFT IS BUSY, UP COMMAND REJECTED");        
+    }
+    else if (fl_sensors.mount_status == true){
+        // Forklift is UP, don't issue command
+        ROS_WARN("FORKLIFT IS UP, UP COMMAND REJECTED");
+    }
+    else{
+        // publish the command to /modbus/regs_write once
+        plc_modbus_node::UInt16Array data;
+        data.name = "forklift";
+        data.data.push_back(forklift_sensors::CMD_LIFT_UP); // Lift Motor Command
+        data.data.push_back(forklift_sensors::CMD_NO_IR); // IR Command
+
+        regs_write.publish(data);
+    }
+
+    return true;
+}
+
+void sensors_callback(const plc_modbus_node::xnergy_sensors::ConstPtr& data){
     xn_sensors = *data;
     // ROS_INFO_STREAM(xn_sensors);
 }
@@ -27,12 +50,11 @@ int main(int argc, char **argv)
     // subscribe to xnergy sensors
     sensors_read = nh.subscribe("/modbus/xnergy_sensors", 100, sensors_callback);
     
-    regs_write = nh.advertise<plc_modbus_node::UInt16Array>("modbus/regs_write", 100);
+    //publish to col write topics
+    coils_write = nh.advertise<plc_modbus_node::ByteArray>("modbus/coils_write", 100);
 
     // advertise xnergy command services
-    /*ros::ServiceServer service_up = nh.advertiseService("forklift_node/forklift_up", forklift_up);
-    ros::ServiceServer service_down = nh.advertiseService("forklift_node/forklift_down", forklift_down);
-    ros::ServiceServer service_ir = nh.advertiseService("forklift_node/forklift_ir", forklift_ir);*/
+    ros::ServiceServer service_up = nh.advertiseService("xnergy_node/charging_services", charge_cmds);
 
     ROS_INFO("Xnergy node is ready");
     ros::spin();
